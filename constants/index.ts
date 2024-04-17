dd7e0b1e6a04bfe9ef7163920299a0ab273e8fd3
Amaan <beingamaan01@gmail.com>
2024-04-17 13:01:30 +0530

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,138 @@
+import { RxHome, RxPerson, RxDashboard, RxClipboard } from "react-icons/rx";
+
+export const SkillData = [
+  {
+    name: "Html 5",
+    Image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Css",
+    Image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "JavaScript",
+    Image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "React",
+    Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Redux",
+    Image: "/redux.png",
+    width: 80,
+    height: 80,
+  },
+
+  {
+    name: "TypeScript",
+    Image: "/ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Next js 13",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Framer Motion",
+    Image: "/framer.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Stripe Payment",
+    Image: "/stripe.webp",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Node js",
+    Image: "/node-js.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Mongo db",
+    Image: "/mongodb.png",
+    width: 40,
+    height: 40,
+  },
+];
+
+export const Socials = [
+  {
+    name: "Discord",
+    src: "/instagram.svg",
+  },
+  {
+    name: "Facebook",
+    src: "/facebook.svg",
+  },
+  {
+    name: "Instagram",
+    src: "/discord.svg",
+  },
+];
+export const Projects = [
+  {
+    
+    title: "Covid-19 Updater",
+    text: "Web application to visualise real time statistics of COVID-19, globally and separately for India.",
+    src: "/covid-web.png",
+  },
+  {
+    title: "ShopNow",
+    text: "Crafted an E-Commerce Web Application incorporated with a payment gateway and a product gallery.",
+    src: "/ShopNow_web.png",
+  },
+  {
+    title: "To-Do List",
+    text: "Implemented local storage functionality within the app for persistent data and enhanced user experience.",
+    src: "/To-do_web.png",
+  },
+  {
+    title: "Talking Dictionary App",
+    text: "Designed and developed the Talking Dictionary, a speech-enabled computer dictionary application providing users with an intuitive way to improve their vocabulary.",
+    src: "/Talking_dict_app.png",
+  },
+];
+
+export const NavLinks = [
+  {
+    name: "/",
+    icon: RxHome,
+    link: "/",
+  },
+  {
+    name: "/my-skills",
+    icon: RxPerson,
+    link: "/my-skills",
+  },
+  {
+    name: "/my-projects",
+    icon: RxDashboard,
+    link: "/my-projects",
+  },
+  {
+    name: "/contact-me",
+    icon: RxClipboard,
+    link: "/contact-me",
+  },
+];
